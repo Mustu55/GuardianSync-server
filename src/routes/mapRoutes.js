117f9e28f66a76c288b2bc5f2bee2c613fe44ca0
unknown <Mustu55@users.noreply.github.com>
2026-09-26 08:36:03 +0530

    Allow operators to bind SVG maps

--- src/routes/mapRoutes.js
+++ src/routes/mapRoutes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { getIndustries, getIndustryByName, updateIndustryNode, uploadBlueprint } = require('../controllers/mapController');
-const { authMiddleware, requireRole } = require('../middleware/authMiddleware');
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.use(authMiddleware);
 
 router.get('/', getIndustries);
 router.get('/:name', getIndustryByName);
-router.post('/upload', requireRole('admin'), uploadBlueprint);
+router.post('/upload', uploadBlueprint);
 router.put('/:name/node', updateIndustryNode);
 
 module.exports = router;
